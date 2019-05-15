413af35cf9260ba8de552828a5c97c9a639349a4
Unknown <wolfla@hotmail.fr>
2019-05-15 13:48:09 +0200

    fix bug on get videos

--- src/app/pages/videos/videos.page.ts
+++ src/app/pages/videos/videos.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Video } from 'src/app/models/video.model';
 import { VideoService } from 'src/app/services/video.service';
 import { Router } from '@angular/router';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-videos',
@@ -14,16 +14,18 @@ export class VideosPage implements OnInit {
   
   public videos: Video[];
 
-  constructor(private videosService: VideoService, private router: Router, public navCtrl: NavController) 
-  { 
-    
+  constructor(private splashScreen: SplashScreen, private videosService: VideoService, private router: Router, public navCtrl: NavController) 
+  {
+
   }
 
   ngOnInit() {
+    this.splashScreen.show();
+
+    this.videosService.getVideos();
     this.videosService.videos.subscribe(
       (videos: Video[]) => {
         this.videos = videos;
-        console.log(videos);
       }
     );
   }
